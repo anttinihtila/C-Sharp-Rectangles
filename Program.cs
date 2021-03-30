baa1ecb7dcc39475076bb3c29905043e3b384a62
Antti Nihtilä <68847740+anttinihtila@users.noreply.github.com>
2021-03-30 13:25:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RectanglesEx
+{
+    class Program : Form
+    {
+        public Program()
+        {
+            InitUI();
+        }
+
+        private void InitUI()
+        {
+            Text = "Rectangles";
+            Paint += new PaintEventHandler(OnPaint);
+
+            ClientSize = new Size(550, 450);
+            CenterToScreen();
+        }
+
+        void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.FillRectangle(Brushes.Violet, 10, 15, 90, 60);
+            g.FillRectangle(Brushes.Indigo, 130, 15, 90, 60);
+            g.FillRectangle(Brushes.Blue, 250, 15, 90, 60);
+            g.FillRectangle(Brushes.LightBlue, 10, 105, 90, 60);
+            g.FillRectangle(Brushes.Green, 130, 105, 90, 60);
+            g.FillRectangle(Brushes.Yellow, 250, 105, 90, 60);
+            g.FillRectangle(Brushes.Orange, 10, 195, 90, 60);
+            g.FillRectangle(Brushes.Red, 130, 195, 90, 60);
+                     
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            // Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.Run(new Program());
+        }
+    }
+}
